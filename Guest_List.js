beea6b23ebdafa86d7565b14a25bfb95b022b554
Tubaarain <160259556+Tubaarain@users.noreply.github.com>
2024-03-13 22:54:43 +0500

    Add files via upload

---
+++ Guest_List.js
@@ -0,0 +1,5 @@
+"use strict";
+let Guest_List = [`Imaran Khan`, `Nawaz Sharif`, `Asif Ali Zerdari`];
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log(`Dear Mr. ` + Guest_List[i] + `,\n\nIt is our pleasuer to invite you in our party.\n\nThank You!`);
+}
---
+++ Guest_List.ts
@@ -0,0 +1,10 @@
+
+
+let Guest_List:string[] = [`Imaran Khan`,`Nawaz Sharif`,`Asif Ali Zerdari`];
+
+for(let i=0; i<Guest_List.length; i++){
+
+    console.log(`Dear Mr. ` + Guest_List[i] + `,\n\nIt is our pleasuer to invite you in our party.\n\nThank You!`)
+
+}
+
