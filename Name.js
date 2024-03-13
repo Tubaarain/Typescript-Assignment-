a8d879cc31cd02a6620482b120f2cd2840d9fad4
Tubaarain <160259556+Tubaarain@users.noreply.github.com>
2024-03-13 22:52:39 +0500

    Add files via upload

---
+++ Name.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = [`Tuba`, `Fattima`, `Noor`, `Raniya`];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ Name.ts
@@ -0,0 +1,10 @@
+
+
+let names:string[] = [`Tuba`,`Fattima`,`Noor`,`Raniya`] ;
+
+
+for (let i=0; i<names.length; i++){
+
+    console.log(names[i]) ;
+
+}
