d5a31359d087c5c9197b2b8eff8f698b0e140d1d
Tubaarain <160259556+Tubaarain@users.noreply.github.com>
2024-03-13 22:55:23 +0500

    Add files via upload

--- Guest_List.js
+++ Guest_List.js
@@ -1,5 +1,12 @@
 "use strict";
 let Guest_List = [`Imaran Khan`, `Nawaz Sharif`, `Asif Ali Zerdari`];
 for (let i = 0; i < Guest_List.length; i++) {
-    console.log(`Dear Mr. ` + Guest_List[i] + `,\n\nIt is our pleasuer to invite you in our party.\n\nThank You!`);
+    console.log(`Dear Mr. ` + Guest_List[i] + `,\n\nIt is our pleasuer to invite you in our party.\n\nThank You!\n\n`);
 }
+let absent_Guest = `Imran Khan`;
+let new_Guest = `Kamran Tessori`;
+Guest_List[0] = new_Guest;
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log(`Dear Mr. ` + Guest_List[i] + `,\n\nIt is our pleasuer to invite you in our party.\n\nThank You!\n\n`);
+}
+console.log(`Mr. ${absent_Guest} is not comingto the party.`);
