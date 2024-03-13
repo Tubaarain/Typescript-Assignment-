f3d25b98bd4e83b73cdad651d671a2cc7ac9f56f
Tubaarain <160259556+Tubaarain@users.noreply.github.com>
2024-03-13 23:25:57 +0500

    Add files via upload

---
+++ Conditinal_Test.js
@@ -0,0 +1,46 @@
+"use strict";
+// Write a series of conditional tests
+// creating car variale
+let car = `subaru`;
+// Test No 1
+console.log("Is car === `subaru? I predict True.");
+console.log(car === `subaru`);
+// Test No 2
+console.log("Is car == `subaru? I predict True.");
+console.log(car == `subaru`);
+// Test No 3
+console.log("Is car != `Honda? I predict True.");
+console.log(car != `Honda`);
+// Test No 4
+console.log("Is car !== `ford? I predict True.");
+console.log(car !== `ford`);
+// Test No 5
+console.log("Is car.Upper case == `SUBARU? I predict True.");
+console.log(car.toUpperCase() == `SUBARU`);
+// Test No 6
+console.log("Is car.lower case == `SUBARU? I predict false.");
+console.log(car.toLowerCase() == `SUBARU`);
+
+// Test No 7
+console.log("Is car.lower case === `SUBARU? I predict false.");
+console.log(car.toLowerCase() === `SUBARU`);
+
+// Test No 8
+console.log("Is car.lower case === `Train? I predict false.");
+console.log(car.toLowerCase() === `Train`);
+
+// Test No 9
+console.log("Is car.lower case === `bus? I predict false.");
+console.log(car.toLowerCase() === `bus`);
+
+// Test No 10
+console.log("Is car.lower case === `Cycle? I predict false.");
+console.log(car.toLowerCase() === `Cycle`);
+
+
+
+// = kisi bhe value ko set/assing kerny k lyn use hota h.
+// == kisi bhe value ko check kerny k lyn use hota h.
+// != Not Equal
+// 2 != 2 true
+// 2 != 2 true
---
+++ Conditinal_Test.ts
@@ -0,0 +1,47 @@
+// Write a series of conditional tests
+
+// creating car variale
+
+let car :string = `subaru`;
+// Test No 1
+console.log("Is car === `subaru? I predict True.");
+console.log(car === `subaru`);
+
+// Test No 2
+console.log("Is car == `subaru? I predict True.");
+console.log(car == `subaru`);
+
+// Test No 3
+console.log("Is car != `Honda? I predict True.");
+console.log(car != `Honda`);
+
+// Test No 4
+console.log("Is car !== `ford? I predict True.");
+console.log(car !== `ford`);
+
+// Test No 5
+console.log("Is car.Upper case == `SUBARU? I predict True.");
+console.log(car.toUpperCase() == `SUBARU`);
+
+// Test No 6
+console.log("Is car.lower case == `SUBARU? I predict false.");
+console.log(car.toLowerCase() == `SUBARU`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// = kisi bhe value ko set/assing kerny k lyn use hota h.
+// == kisi bhe value ko check kerny k lyn use hota h.
+// != Not Equal
+// 2 != 2 true
+// 2 != 2 true
