a636d243097466d8862d011dd22bdd5489933d8a
Tubaarain <160259556+Tubaarain@users.noreply.github.com>
2024-03-13 23:24:45 +0500

    Add files via upload

---
+++ Typescript_project.js
@@ -0,0 +1,19 @@
+"use strict";
+// Write a program that creates Objects containing these items
+// person object.
+let person = {
+    age: 26,
+    name: `Taha`,
+    nationality: `Pakistan`,
+    Student: true,
+};
+// print object.
+console.log(person);
+// car object.
+let car = {
+    maker: `toyota`,
+    color: `Black`,
+    automatic: `true`,
+};
+// print car.
+console.log(car);
