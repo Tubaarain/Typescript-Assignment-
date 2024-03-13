e089ed8fa338f0555be67de987bc9a97a41ba789
Tubaarain <160259556+Tubaarain@users.noreply.github.com>
2024-03-13 22:53:22 +0500

    Add files via upload

--- Name.js
+++ Name.js
@@ -1,5 +1,6 @@
 "use strict";
 let names = [`Tuba`, `Fattima`, `Noor`, `Raniya`];
+const message = `well come to typescript class: `;
 for (let i = 0; i < names.length; i++) {
-    console.log(names[i]);
+    console.log(message + names[i]);
 }
--- Name.ts
+++ Name.ts
@@ -1,10 +1,11 @@
 
 
-let names:string[] = [`Tuba`,`Fattima`,`Noor`,`Raniya`] ;
+let names :string[] = [`Tuba`,`Fattima`,`Noor`,`Raniya`] ;
 
+const message :string = `well come to typescript class: `
 
 for (let i=0; i<names.length; i++){
 
-    console.log(names[i]) ;
+    console.log(message + names[i]) ;
 
 }
