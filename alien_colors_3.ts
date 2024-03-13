2eb9d878810749abbe8f395469c8674d69fb7893
Tubaarain <160259556+Tubaarain@users.noreply.github.com>
2024-03-13 23:33:43 +0500

    Add files via upload

---
+++ alien_colors_3.ts
@@ -0,0 +1,47 @@
+let alien_Colors = "green";
+// • If the alien is red, print a message that the player earned 15 points.
+//  • If the alien is yellow, print a message that the player earned 10 point.
+// If the alien is green, print a message that the player earned 5 points.
+// • Write three versions of this program, making sure each message is printed for the appropriate color alien.
+//  version 1 of the program
+if (alien_Colors === "green") {
+    console.log("playre earned 5 points.");
+}
+else if (alien_Colors === " yellow") {
+    console.log("player earned 10 points.");
+}
+else if (alien_Colors === " red") {
+    console.log("player earned 15 points.");
+}
+else {
+    console.log("please select right color");
+}
+// version 2 of the program
+ let alien_color = "yellow" ;
+if (alien_Colors === "green") {
+    console.log("playre earned 10 points.");
+}
+else if (alien_Colors === " yellow") {
+    console.log("player earned 10 points.");
+}
+else if (alien_Colors === " red") {
+    console.log("player earned 15 points.");
+}
+else {
+    console.log("please select right color");
+}
+// version 3 of the program
+ let alien_Color = "red" ;
+if (alien_Colors === "green") {
+    console.log("playre earned 15 points.");
+}
+else if (alien_Colors === " yellow") {
+    console.log("player earned 10 points.");
+}
+else if (alien_Colors === " red") {
+    console.log("player earned 15 points.");
+}
+else {
+    console.log("please select right color");
+}
+
