6752228e2b8c1f851b85e3a91072fec4d099351e
Tubaarain <160259556+Tubaarain@users.noreply.github.com>
2024-03-13 23:32:50 +0500

    Add files via upload

---
+++ alien_color_2.js
@@ -0,0 +1,19 @@
+"use strict";
+// • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+let alienColors = "green";
+// write an if -else chain.
+if (alienColors === "green") {
+    console.log("player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("player just earned 10 points.");
+}
+// • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+alienColors = "Yellow";
+if (alienColors === "green") {
+    console.log("player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("player just earned 10 points.");
+}
+// • Write one version of this program that runs the if block and another that runs the else block
---
+++ alien_color_2.ts
@@ -0,0 +1,21 @@
+
+// • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+let alienColors: string = "green";
+// write an if -else chain.
+if(alienColors === "green"){
+    console.log("player just earned 5 points for shooting the alien.")
+}else{
+    console.log("player just earned 10 points.")
+}
+
+// • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+
+alienColors = "Yellow"
+
+if(alienColors === "green"){
+    console.log("player just earned 5 points for shooting the alien.")
+}else{
+    console.log("player just earned 10 points.")
+}
+
+// • Write one version of this program that runs the if block and another that runs the else block
